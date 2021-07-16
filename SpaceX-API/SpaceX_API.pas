2e13e2bfdb06590715c81c9de6792798b77b6efc
Cornelius Donley <donley@live.com>
2021-07-16 11:02:26 -0500

    Initial

---
+++ SpaceX-API/SpaceX_API.pas
@@ -0,0 +1,77 @@
+program SpaceX_Console;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  Classes, SysUtils, CustApp
+  { you can add units after this };
+
+type
+
+  { SpaceX }
+
+  SpaceX = class(TCustomApplication)
+  protected
+    procedure DoRun; override;
+  public
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure WriteHelp; virtual;
+  end;
+
+{ SpaceX }
+
+procedure SpaceX.DoRun;
+var
+  ErrorMsg: String;
+begin
+  // quick check parameters
+  ErrorMsg:=CheckOptions('h', 'help');
+  if ErrorMsg<>'' then begin
+    ShowException(Exception.Create(ErrorMsg));
+    Terminate;
+    Exit;
+  end;
+
+  // parse parameters
+  if HasOption('h', 'help') then begin
+    WriteHelp;
+    Terminate;
+    Exit;
+  end;
+
+  { add your program here }
+
+  // stop program loop
+  Terminate;
+end;
+
+constructor SpaceX.Create(TheOwner: TComponent);
+begin
+  inherited Create(TheOwner);
+  StopOnException:=True;
+end;
+
+destructor SpaceX.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure SpaceX.WriteHelp;
+begin
+  { add your help code here }
+  writeln('Usage: ', ExeName, ' -h');
+end;
+
+var
+  Application: SpaceX;
+begin
+  Application:=SpaceX.Create(nil);
+  Application.Title:='SpaceX';
+  Application.Run;
+  Application.Free;
+end.
+
