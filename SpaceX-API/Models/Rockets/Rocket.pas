978328dba9cbc03b8732e51be7d89931e7491a93
Kyle Loveless <kjloveless321@gmail.com>
2021-07-29 11:04:24 -0500

    Convert Rocket and RocketPotentialPaayloadWeight to interfce.

--- SpaceX-API/Models/Rockets/Rocket.pas
+++ SpaceX-API/Models/Rockets/Rocket.pas
@@ -5,46 +5,395 @@ unit Rocket;
 interface
 
 uses
-  Classes, SysUtils, SizeInfo, MassInfo, RocketEngines, RocketFirstStage, SecondStage, RocketLandingLegs;
+  Classes, SysUtils, SizeInfo, MassInfo, RocketEngines, RocketFirstStage, SecondStage, RocketLandingLegs,
+  RocketPotentialPayloadWeight;
+
+type
+
+  IBaseRocket = interface(IInterface) ['{06F97F32-2431-4FDE-95C1-D3EF85A5F133}']
+    function GetActive: Boolean;
+    function GetBoosters: LongWord;
+    function GetCompany: string;
+    function GetCostPerLaunch: LongWord;
+    function GetCountry: string;
+    function GetDescription: string;
+    function GetDiameter: ISizeInfo;
+    function GetEngines: TRocketEngines;
+    function GetFirstFlight: TDateTime;
+    function GetFirstStage: TRocketFirstStage;
+    function GetFlickrImages: TStringList;
+    function GetHeight: ISizeInfo;
+    function GetId: string;
+    function GetLandingLegs: TRocketLandingLegs;
+    function GetMass: IMassInfo;
+    function GetName: string;
+    function GetPayloadWeights: IRocketPotentialPayloadWeightList;
+    function GetSecondStage: TSecondStage;
+    function GetStages: LongWord;
+    function GetSuccessRate: LongWord;
+    function GetTypeInfo: string;
+    function GetWikipedia: string;
+
+    procedure SetActive(AValue: Boolean);
+    procedure SetBoosters(AValue: LongWord);
+    procedure SetCompany(AValue: string);
+    procedure SetCostPerLaunch(AValue: LongWord);
+    procedure SetCountry(AValue: string);
+    procedure SetDescription(AValue: string);
+    procedure SetDiameter(AValue: ISizeInfo);
+    procedure SetEngines(AValue: TRocketEngines);
+    procedure SetFirstFlight(AValue: TDateTime);
+    procedure SetFirstStage(AValue: TRocketFirstStage);
+    procedure SetFlickrImages(AValue: TStringList);
+    procedure SetHeight(AValue: ISizeInfo);
+    procedure SetId(AValue: string);
+    procedure SetLandingLegs(AValue: TRocketLandingLegs);
+    procedure SetMass(AValue: IMassInfo);
+    procedure SetName(AValue: string);
+    procedure SetPayloadWeights(AValue: IRocketPotentialPayloadWeightList);
+    procedure SetSecondStage(AValue: TSecondStage);
+    procedure SetStages(AValue: LongWord);
+    procedure SetSuccessRate(AValue: LongWord);
+    procedure SetTypeInfo(AValue: string);
+    procedure SetWikipedia(AValue: string);
+  end;
+
+  IRocket = interface(IBaseRocket) ['{63AC2BDA-F8F5-4E2A-AB0D-2D1D939DBABF}']
+    property Active: Boolean read GetActive write SetActive;
+    property Boosters: LongWord read GetBoosters write SetBoosters;
+    property Company: string read GetCompany write SetCompany;
+    property CostPerLaunch: LongWord read GetCostPerLaunch write SetCostPerLaunch;
+    property Country: string read GetCountry write SetCountry;
+    property Description: string read GetDescription write SetDescription;
+    property Diameter: ISizeInfo read GetDiameter write SetDiameter;
+    property Engines: TRocketEngines read GetEngines write SetEngines;
+    property FirstFlight: TDateTime read GetFirstFlight write SetFirstFlight;
+    property FirstStage: TRocketFirstStage read GetFirstStage write SetFirstStage;
+    property FlickrImages: TStringList read GetFlickrImages write SetFlickrImages;
+    property Height: ISizeInfo read GetHeight write SetHeight;
+    property Id: string read GetId write SetId;
+    property LandingLegs: TRocketLandingLegs read GetLandingLegs write SetLandingLegs;
+    property Mass: IMassInfo read GetMass write SetMass;
+    property Name: string read GetName write SetName;
+    property PayloadWeights: IRocketPotentialPayloadWeightList read GetPayloadWeights write SetPayloadWeights;
+    property SecondStage: TSecondStage read GetSecondStage write SetSecondStage;
+    property Stages: LongWord read GetStages write SetStages;
+    property SuccessRate: LongWord read GetSuccessRate write SetSuccessRate;
+    property TypeInfo: string read GetTypeInfo write SetTypeInfo;
+    property Wikipedia: string read GetWikipedia write SetWikipedia;
+  end;
+
+function NewRocket: IRocket;
+
+implementation
 
 type
 
   { TRocket }
 
-  TRocket = class(TInterfacedObject)
+  TRocket = class(TInterfacedObject, IRocket)
+  private
+    FActive: Boolean;
+    FBoosters: LongWord;
+    FCompany: string;
+    FCostPerLaunch: LongWord;
+    FCountry: string;
+    FDescription: string;
+    FDiameter: ISizeInfo;
+    FEngines: TRocketEngines;
+    FFirstFlight: TDateTime;
+    FFirstStage: TRocketFirstStage;
+    FFlickrImages: TStringList;
+    FHeight: ISizeInfo;
+    FId: string;
+    FLandingLegs: TRocketLandingLegs;
+    FMass: IMassInfo;
+    FName: string;
+    FPayloadWeights: IRocketPotentialPayloadWeightList;
+    FSecondStage: TSecondStage;
+    FStages: LongWord;
+    FSuccessRate: LongWord;
+    FTypeInfo: string;
+    FWikipedia: string;
+    function GetActive: Boolean;
+    function GetBoosters: LongWord;
+    function GetCompany: string;
+    function GetCostPerLaunch: LongWord;
+    function GetCountry: string;
+    function GetDescription: string;
+    function GetDiameter: ISizeInfo;
+    function GetEngines: TRocketEngines;
+    function GetFirstFlight: TDateTime;
+    function GetFirstStage: TRocketFirstStage;
+    function GetFlickrImages: TStringList;
+    function GetHeight: ISizeInfo;
+    function GetId: string;
+    function GetLandingLegs: TRocketLandingLegs;
+    function GetMass: IMassInfo;
+    function GetName: string;
+    function GetPayloadWeights: IRocketPotentialPayloadWeightList;
+    function GetSecondStage: TSecondStage;
+    function GetStages: LongWord;
+    function GetSuccessRate: LongWord;
+    function GetTypeInfo: string;
+    function GetWikipedia: string;
+
+    procedure SetActive(AValue: Boolean);
+    procedure SetBoosters(AValue: LongWord);
+    procedure SetCompany(AValue: string);
+    procedure SetCostPerLaunch(AValue: LongWord);
+    procedure SetCountry(AValue: string);
+    procedure SetDescription(AValue: string);
+    procedure SetDiameter(AValue: ISizeInfo);
+    procedure SetEngines(AValue: TRocketEngines);
+    procedure SetFirstFlight(AValue: TDateTime);
+    procedure SetFirstStage(AValue: TRocketFirstStage);
+    procedure SetFlickrImages(AValue: TStringList);
+    procedure SetHeight(AValue: ISizeInfo);
+    procedure SetId(AValue: string);
+    procedure SetLandingLegs(AValue: TRocketLandingLegs);
+    procedure SetMass(AValue: IMassInfo);
+    procedure SetName(AValue: string);
+    procedure SetPayloadWeights(AValue: IRocketPotentialPayloadWeightList);
+    procedure SetSecondStage(AValue: TSecondStage);
+    procedure SetStages(AValue: LongWord);
+    procedure SetSuccessRate(AValue: LongWord);
+    procedure SetTypeInfo(AValue: string);
+    procedure SetWikipedia(AValue: string);
   public
-    Id: string;
-    Name: string;
-    TypeInfo: string;
-    Active: Boolean;
-    Stages: LongWord;
-    Boosters: LongWord;
-    Country: string;
-    Company: string;
-    Wikipedia: string;
-    Description: string;
-    Height: ISizeInfo;
-    Diameter: ISizeInfo;
-    Mass: IMassInfo;
-    Engines: TRocketEngines;
-    CostPerLaunch: LongWord;
-    SuccessRate: LongWord;
-    FirstFlight: TDateTime;
-    FirstStage: TRocketFirstStage;
-    SecondStage: TSecondStage;
-    LandingLegs: TRocketLandingLegs;
-    //PayloadWeights: List<RocketPotentialPayloadWeight>;
-    FlickrImages: TStringList;
     function ToString(): string; override;
   end;
 
-implementation
+function NewRocket: IRocket;
+begin
+  Result := TRocket.Create;
+end;
 
 { TRocket }
 
+function TRocket.GetActive: Boolean;
+begin
+  Result := FActive;
+end;
+
+function TRocket.GetBoosters: LongWord;
+begin
+  Result := FBoosters;
+end;
+
+function TRocket.GetCompany: string;
+begin
+  Result := FCompany;
+end;
+
+function TRocket.GetCostPerLaunch: LongWord;
+begin
+  Result := FCostPerLaunch;
+end;
+
+function TRocket.GetCountry: string;
+begin
+  Result := FCountry;
+end;
+
+function TRocket.GetDescription: string;
+begin
+  Result := FDescription;
+end;
+
+function TRocket.GetDiameter: ISizeInfo;
+begin
+  Result := FDiameter;
+end;
+
+function TRocket.GetEngines: TRocketEngines;
+begin
+  Result := FEngines;
+end;
+
+function TRocket.GetFirstFlight: TDateTime;
+begin
+  Result := FFirstFlight;
+end;
+
+function TRocket.GetFirstStage: TRocketFirstStage;
+begin
+  Result := FFirstStage;
+end;
+
+function TRocket.GetFlickrImages: TStringList;
+begin
+  Result := FFlickrImages;
+end;
+
+function TRocket.GetHeight: ISizeInfo;
+begin
+  Result := FHeight;
+end;
+
+function TRocket.GetId: string;
+begin
+  Result := FId;
+end;
+
+function TRocket.GetLandingLegs: TRocketLandingLegs;
+begin
+  Result := FLandingLegs;
+end;
+
+function TRocket.GetMass: IMassInfo;
+begin
+  Result := FMass;
+end;
+
+function TRocket.GetName: string;
+begin
+  Result := FName;
+end;
+
+function TRocket.GetPayloadWeights: IRocketPotentialPayloadWeightList;
+begin
+  Result := FPayloadWeights;
+end;
+
+function TRocket.GetSecondStage: TSecondStage;
+begin
+  Result := FSecondStage;
+end;
+
+function TRocket.GetStages: LongWord;
+begin
+  Result := FStages;
+end;
+
+function TRocket.GetSuccessRate: LongWord;
+begin
+  Result := FSuccessRate;
+end;
+
+function TRocket.GetTypeInfo: string;
+begin
+  Result := FTypeInfo;
+end;
+
+function TRocket.GetWikipedia: string;
+begin
+  Result := FWikipedia;
+end;
+
+procedure TRocket.SetActive(AValue: Boolean);
+begin
+  FActive := AValue;
+end;
+
+procedure TRocket.SetBoosters(AValue: LongWord);
+begin
+  FBoosters := AValue;
+end;
+
+procedure TRocket.SetCompany(AValue: string);
+begin
+  FCompany := AValue;
+end;
+
+procedure TRocket.SetCostPerLaunch(AValue: LongWord);
+begin
+  FCostPerLaunch := AValue;
+end;
+
+procedure TRocket.SetCountry(AValue: string);
+begin
+  FCountry := AValue;
+end;
+
+procedure TRocket.SetDescription(AValue: string);
+begin
+  FDescription := AValue;
+end;
+
+procedure TRocket.SetDiameter(AValue: ISizeInfo);
+begin
+  FDiameter := AValue;
+end;
+
+procedure TRocket.SetEngines(AValue: TRocketEngines);
+begin
+  FEngines := AValue;
+end;
+
+procedure TRocket.SetFirstFlight(AValue: TDateTime);
+begin
+  FFirstFlight := AValue;
+end;
+
+procedure TRocket.SetFirstStage(AValue: TRocketFirstStage);
+begin
+  FFirstStage := AValue;
+end;
+
+procedure TRocket.SetFlickrImages(AValue: TStringList);
+begin
+  FFlickrImages := AValue;
+end;
+
+procedure TRocket.SetHeight(AValue: ISizeInfo);
+begin
+  FHeight := AValue;
+end;
+
+procedure TRocket.SetId(AValue: string);
+begin
+  FId := AValue;
+end;
+
+procedure TRocket.SetLandingLegs(AValue: TRocketLandingLegs);
+begin
+  FLandingLegs := AValue;
+end;
+
+procedure TRocket.SetMass(AValue: IMassInfo);
+begin
+  FMass := AValue;
+end;
+
+procedure TRocket.SetName(AValue: string);
+begin
+  FName := AValue;
+end;
+
+procedure TRocket.SetPayloadWeights(AValue: IRocketPotentialPayloadWeightList);
+begin
+  FPayloadWeights := AValue;
+end;
+
+procedure TRocket.SetSecondStage(AValue: TSecondStage);
+begin
+  FSecondStage := AValue;
+end;
+
+procedure TRocket.SetStages(AValue: LongWord);
+begin
+  FStages := AValue;
+end;
+
+procedure TRocket.SetSuccessRate(AValue: LongWord);
+begin
+  FSuccessRate := AValue;
+end;
+
+procedure TRocket.SetTypeInfo(AValue: string);
+begin
+  FTypeInfo := AValue;
+end;
+
+procedure TRocket.SetWikipedia(AValue: string);
+begin
+  FWikipedia := AValue;
+end;
+
 function TRocket.ToString(): string;
 begin
-  Result := Name;
+  Result := GetName;
 end;
 
 end.
--- SpaceX-API/Models/Rockets/RocketPotentialPayloadWeight.pas
+++ SpaceX-API/Models/Rockets/RocketPotentialPayloadWeight.pas
@@ -8,16 +8,109 @@ uses
   Classes, SysUtils;
 
 type
-  TRocketPotentialPayloadWeight = class(TInterfacedObject)
-  public
-    Id: string;
-    Name: string;
-    Kilograms: Double;
-    Pounds: Double;
+
+  IBaseRocketPotentialPayloadWeight = interface(IInterface) ['{5A89D92B-A810-40AF-9FC6-DB5FC1E20854}']
+    function GetId: string;
+    function GetKilograms: Double;
+    function GetName: string;
+    function GetPounds: Double;
+
+    procedure SetId(AValue: string);
+    procedure SetKilograms(AValue: Double);
+    procedure SetName(AValue: string);
+    procedure SetPounds(AValue: Double);
   end;
 
+  IRocketPotentialPayloadWeight = interface(IBaseRocketPotentialPayloadWeight) ['{63F840A2-75BA-476B-B7EF-D83BD0956654}']
+    property Id: string read GetId write SetId;
+    property Kilograms: Double read GetKilograms write SetKilograms;
+    property Name: string read GetName write SetName;
+    property Pounds: Double read GetPounds write SetPounds;
+  end;
+
+  IRocketPotentialPayloadWeightList = interface(IInterfaceList) ['{9BD75D9F-F862-49DA-ABA8-22A78A860D3E}']
+  end;
+
+function NewRocketPotentialPayloadWeight: IRocketPotentialPayloadWeight;
+function NewRocketPotentialPayloadWeightList: IRocketPotentialPayloadWeightList;
 
 implementation
+
+type
+
+  { TRocketPotentialPayloadWeight }
+
+  TRocketPotentialPayloadWeight = class(TInterfacedObject, IRocketPotentialPayloadWeight)
+  private
+    FId: string;
+    FKilograms: Double;
+    FName: string;
+    FPounds: Double;
+    function GetId: string;
+    function GetKilograms: Double;
+    function GetName: string;
+    function GetPounds: Double;
+
+    procedure SetId(AValue: string);
+    procedure SetKilograms(AValue: Double);
+    procedure SetName(AValue: string);
+    procedure SetPounds(AValue: Double);
+  end;
+
+  TRocketPotentialPayloadWightList = class(TInterfaceList, IRocketPotentialPayloadWeightList)
+  end;
+
+function NewRocketPotentialPayloadWeight: IRocketPotentialPayloadWeight;
+begin
+  Result := TRocketPotentialPayloadWeight.Create;
+end;
+
+function NewRocketPotentialPayloadWeightList: IRocketPotentialPayloadWeightList;
+begin
+  Result := TRocketPotentialPayloadWightList.Create;
+end;
+
+{ TRocketPotentialPayloadWeight }
+
+function TRocketPotentialPayloadWeight.GetId: string;
+begin
+  Result := FId;
+end;
+
+function TRocketPotentialPayloadWeight.GetKilograms: Double;
+begin
+  Result := FKilograms;
+end;
+
+function TRocketPotentialPayloadWeight.GetName: string;
+begin
+  Result := FName;
+end;
+
+function TRocketPotentialPayloadWeight.GetPounds: Double;
+begin
+  Result := FPounds;
+end;
+
+procedure TRocketPotentialPayloadWeight.SetId(AValue: string);
+begin
+  FId := AValue;
+end;
+
+procedure TRocketPotentialPayloadWeight.SetKilograms(AValue: Double);
+begin
+  FKilograms := AValue;
+end;
+
+procedure TRocketPotentialPayloadWeight.SetName(AValue: string);
+begin
+  FName := AValue;
+end;
+
+procedure TRocketPotentialPayloadWeight.SetPounds(AValue: Double);
+begin
+  FPounds := AValue;
+end;
 
 end.
 
